a6d712077e2fe8e19e41b3b905ca066dbe05a4c9
Daniel Vitor <danielvitorlog221@gmail.com>
2023-02-25 12:02:57 -0300

    Movimentação do chao e som

--- jogo.js
+++ jogo.js
@@ -1,3 +1,6 @@
+const som_HIT = new Audio()
+som_HIT.src = './efeitos/hit.wav'
+
 const sprites = new Image();
 sprites.src = './sprites.png';
 
@@ -35,55 +38,87 @@ const planoDeFundo = {
 }
 
 // Chão
-const chao = {
-  spritesX: 0,
-  spritesY: 610,
-  largura: 224,
-  altura: 112,
-  x: 0,
-  y: canvas.height - 112,
-  desenha() {
-    contexto.drawImage(
-      sprites,
-      chao.spritesX, chao.spritesY,
-      chao.largura, chao.altura,
-      chao.x, chao.y,
-      chao.largura, chao.altura
-    )
+function criaChao() {
+  const chao = {
+    spritesX: 0,
+    spritesY: 610,
+    largura: 224,
+    altura: 112,
+    x: 0,
+    y: canvas.height - 112,
+    atualiza() {
+      const movimentoDoChao = 1
+      const repeteEm = chao.largura / 2 
+      const movimentacao = chao.x = chao.x - movimentoDoChao
 
-    contexto.drawImage(
-      sprites,
-      chao.spritesX, chao.spritesY,
-      chao.largura, chao.altura,
-      (chao.x + chao.largura), chao.y,
-      chao.largura, chao.altura
-    )
+      chao.x = movimentacao % repeteEm
+    },
+    desenha() {
+      contexto.drawImage(
+        sprites,
+        chao.spritesX, chao.spritesY,
+        chao.largura, chao.altura,
+        chao.x, chao.y,
+        chao.largura, chao.altura
+      )
+  
+      contexto.drawImage(
+        sprites,
+        chao.spritesX, chao.spritesY,
+        chao.largura, chao.altura,
+        (chao.x + chao.largura), chao.y,
+        chao.largura, chao.altura
+      )
+    }
   }
+  return chao
+}
+
+function fazColisao(flappyBird, chao) {
+  const flappyBirdY = flappyBird.y + flappyBird.altura
+  const chaoY = chao.y
+
+  if(flappyBirdY >= chaoY) {
+    return true
+  }
+  return false
 }
 
 // Passaro
-const flappyBird = {
-  spritesX: 0,
-  spritesY: 0,
-  largura: 33,
-  altura: 24,
-  x: 10,
-  y: 50,
-  gravidade: 0.25,
-  velocidade: 0,
-  atualiza() {
-    flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade
-    flappyBird.y = flappyBird.y + flappyBird.velocidade
-  },
-  desenha() {
-    contexto.drawImage(
-      sprites,
-      flappyBird.spritesX, flappyBird.spritesY,
-      flappyBird.largura, flappyBird.altura,
-      flappyBird.x, flappyBird.y,
-      flappyBird.largura, flappyBird.altura
-    )
+function criaFlappyBird() {
+  const flappyBird = {
+    spritesX: 0,
+    spritesY: 0,
+    largura: 33,
+    altura: 24,
+    x: 10,
+    y: 50,
+    gravidade: 0.25,
+    velocidade: 0,
+    pulo: 4.6,
+    atualiza() {
+      if(fazColisao(flappyBird, chao)) {
+        som_HIT.play()
+        mudaParaTela(Telas.INICIO)
+        return
+      }
+      flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade
+      flappyBird.y = flappyBird.y + flappyBird.velocidade
+    },
+    desenha() {
+      contexto.drawImage(
+        sprites,
+        flappyBird.spritesX, flappyBird.spritesY,
+        flappyBird.largura, flappyBird.altura,
+        flappyBird.x, flappyBird.y,
+        flappyBird.largura, flappyBird.altura
+      )
+    },
+    pula() {
+      flappyBird.velocidade = - flappyBird.pulo
+    }
   }
+  return flappyBird
 }
 
 // Tela de get ready
@@ -108,23 +143,33 @@ const mensagemGetReady = {
 //
 // Telas
 //
+const globais = {}
 let telaAtiva = {}
 function mudaParaTela(novaTela) {
   telaAtiva = novaTela
+
+  if(telaAtiva.inicializa) {
+    telaAtiva.inicializa()
+  }
 }
+
 const Telas = {
   INICIO: {
+    inicializa() {
+      globais.flappyBird = criaFlappyBird()
+      globais.chao = criaChao()
+    },
     desenha() {
       planoDeFundo.desenha()
-      chao.desenha()
-      flappyBird.desenha()
+      globais.chao.desenha()
+      globais.flappyBird.desenha()
       mensagemGetReady.desenha()
     },
     click() {
       mudaParaTela(Telas.JOGO)
     },
     atualiza() {
-
+      globais.chao.atualiza()
     }
   }
 }
@@ -132,11 +177,14 @@ const Telas = {
 Telas.JOGO = {
   desenha() {
     planoDeFundo.desenha()
-    chao.desenha()
-    flappyBird.desenha()
+    globais.chao.desenha()
+    globais.flappyBird.desenha()
+  },
+  click() {
+    globais.flappyBird.pula()
   },
   atualiza() {
-    flappyBird.atualiza()
+    globais.flappyBird.atualiza()
   }
 }
 
