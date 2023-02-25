43f874866a5b7f09edbb0aaf73f6ca9d50adca64
Daniel Vitor <danielvitorlog221@gmail.com>
2023-02-25 10:29:21 -0300

    Inicio do game(fundo e queda do passarinho)

--- jogo.js
+++ jogo.js
@@ -1,46 +1,43 @@
-console.log('[DevSoutinho] Flappy Bird');
-
 const sprites = new Image();
 sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
-
-// [Plano de Fundo]
+// PLano de fundo
 const planoDeFundo = {
-  spriteX: 390,
-  spriteY: 0,
+  spritesX: 390,
+  spritesY: 0,
   largura: 275,
   altura: 204,
   x: 0,
   y: canvas.height - 204,
   desenha() {
-    contexto.fillStyle = '#70c5ce';
-    contexto.fillRect(0,0, canvas.width, canvas.height)
+    contexto.fillStyle = '#70c5ce'
+    contexto.fillRect(0, 0, canvas.width, canvas.height)
 
     contexto.drawImage(
       sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
+      planoDeFundo.spritesX, planoDeFundo.spritesY,
       planoDeFundo.largura, planoDeFundo.altura,
       planoDeFundo.x, planoDeFundo.y,
-      planoDeFundo.largura, planoDeFundo.altura,
-    );
+      planoDeFundo.largura, planoDeFundo.altura
+    )
 
     contexto.drawImage(
       sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
+      planoDeFundo.spritesX, planoDeFundo.spritesY,
       planoDeFundo.largura, planoDeFundo.altura,
       (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
-      planoDeFundo.largura, planoDeFundo.altura,
-    );
-  },
-};
+      planoDeFundo.largura, planoDeFundo.altura
+    )
+  }
+}
 
-// [Chao]
+// Chão
 const chao = {
-  spriteX: 0,
-  spriteY: 610,
+  spritesX: 0,
+  spritesY: 610,
   largura: 224,
   altura: 112,
   x: 0,
@@ -48,48 +45,53 @@ const chao = {
   desenha() {
     contexto.drawImage(
       sprites,
-      chao.spriteX, chao.spriteY,
+      chao.spritesX, chao.spritesY,
       chao.largura, chao.altura,
       chao.x, chao.y,
-      chao.largura, chao.altura,
-    );
+      chao.largura, chao.altura
+    )
 
     contexto.drawImage(
       sprites,
-      chao.spriteX, chao.spriteY,
+      chao.spritesX, chao.spritesY,
       chao.largura, chao.altura,
       (chao.x + chao.largura), chao.y,
-      chao.largura, chao.altura,
-    );
-  },
-};
+      chao.largura, chao.altura
+    )
+  }
+}
 
+// Passaro
 const flappyBird = {
-  spriteX: 0,
-  spriteY: 0,
+  spritesX: 0,
+  spritesY: 0,
   largura: 33,
   altura: 24,
   x: 10,
   y: 50,
+  gravidade: 0.25,
+  velocidade: 0,
+  atualiza() {
+    flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade
+    flappyBird.y = flappyBird.y + flappyBird.velocidade
+  },
   desenha() {
     contexto.drawImage(
       sprites,
-      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
-      flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
-      flappyBird.x, flappyBird.y,
+      flappyBird.spritesX, flappyBird.spritesY,
       flappyBird.largura, flappyBird.altura,
-    );
+      flappyBird.x, flappyBird.y,
+      flappyBird.largura, flappyBird.altura
+    )
   }
 }
 
 function loop() {
-  planoDeFundo.desenha();
-  chao.desenha();
-  flappyBird.desenha();
+  flappyBird.atualiza()
 
-  flappyBird.y = flappyBird.y + 1;
-
-  requestAnimationFrame(loop);
+  planoDeFundo.desenha()
+  chao.desenha()
+  flappyBird.desenha()
+  requestAnimationFrame(loop)
 }
-
-loop();
+loop()
